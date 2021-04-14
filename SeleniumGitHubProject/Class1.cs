06f6be839527ae5f08c7a4484856cbeb0a1f4ccd
Norah Lovelace <call1800norah@gmail.com>
2021-04-14 14:04:10 -0500

    Add project files.

---
+++ SeleniumGitHubProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumGitHubProject
+{
+    public class Class1
+    {
+    }
+}
